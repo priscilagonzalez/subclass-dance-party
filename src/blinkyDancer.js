fd5f20caaed2004927bb67139ffb35c44c9ac43f
Priscila <priscila.oliveiraa@gmail.com>
2019-12-17 00:08:15 -0500

    Refactoring the Dancer class and its subclass BlinkyDancer from functional inheritance to pseudoclassical.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,10 +1,10 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
-  // var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 };
 
 // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
+// so we must keep a copy of the old version of this function
 
 //var oldStep = blinkyDancer.step;
 
@@ -22,5 +22,3 @@ BlinkyDancer.prototype.step = function() {
   console.log('this', this);
   this.$node.toggle();
 };
-
-

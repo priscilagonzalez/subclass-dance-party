8c6483acc891f4f81a8fab195a07f102e92b5349
Priscila <priscila.oliveiraa@gmail.com>
2019-12-17 22:29:08 -0500

    Deleted some comments.

--- src/init.js
+++ src/init.js
@@ -16,26 +16,10 @@ $(document).ready(function() {
      * to the stage.
      */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-    console.log('dancerMakerFunctionName', dancerMakerFunctionName);
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-    // console.log('dancerMakerFunction', dancerMakerFunction);
 
-    // var happyDancerMakerFunctionName = $(this).data('dancer-happy-maker-function-name');
-    // console.log('dancerMakerFunctionName', happyDancerMakerFunctionName);
-
-    // var happyDancerMakerFunction = window[happyDancerMakerFunctionName];
-    // console.log('dancerMakerFunction', happyDancerMakerFunction);
-
-    // var oldDancerMakerFunctionName = $(this).data('dancer-old-maker-function-name');
-    // console.log('oldDancerMakerFunctionName', oldDancerMakerFunctionName);
-
-    // get the maker function for the kind of dancer we're supposed to make
-    // var oldDancerMakerFunction = window[oldDancerMakerFunctionName];
-    // console.log('oldDancerMakerFunction', oldDancerMakerFunction);
-
-    // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
@@ -46,96 +30,32 @@ $(document).ready(function() {
   });
 
   $('.addOldDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
 
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-    console.log('dancerMakerFunctionName', dancerMakerFunctionName);
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-    // console.log('dancerMakerFunction', dancerMakerFunction);
-
-    // var happyDancerMakerFunctionName = $(this).data('dancer-happy-maker-function-name');
-    // console.log('dancerMakerFunctionName', happyDancerMakerFunctionName);
-
-    // var happyDancerMakerFunction = window[happyDancerMakerFunctionName];
-    // console.log('dancerMakerFunction', happyDancerMakerFunction);
-
-    // var oldDancerMakerFunctionName = $(this).data('dancer-old-maker-function-name');
-    // console.log('oldDancerMakerFunctionName', oldDancerMakerFunctionName);
-
-    // get the maker function for the kind of dancer we're supposed to make
-    // var oldDancerMakerFunction = window[oldDancerMakerFunctionName];
-    // console.log('oldDancerMakerFunction', oldDancerMakerFunction);
-
-    // make a dancer with a random position
-
     var oldDancer = new dancerMakerFunction(
-      20,
-      50,
-      1000
-      // $("body").height() * Math.random(),
-      // $("body").width() * Math.random(),
-      // Math.random() * 1000
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
     );
 
     $('body').append(oldDancer.$node);
   });
 
   $('.addHappyDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
 
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-    console.log('dancerMakerFunctionName', dancerMakerFunctionName);
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-    // console.log('dancerMakerFunction', dancerMakerFunction);
-
-    // var happyDancerMakerFunctionName = $(this).data('dancer-happy-maker-function-name');
-    // console.log('dancerMakerFunctionName', happyDancerMakerFunctionName);
-
-    // var happyDancerMakerFunction = window[happyDancerMakerFunctionName];
-    // console.log('dancerMakerFunction', happyDancerMakerFunction);
-
-    // var oldDancerMakerFunctionName = $(this).data('dancer-old-maker-function-name');
-    // console.log('oldDancerMakerFunctionName', oldDancerMakerFunctionName);
-
-    // get the maker function for the kind of dancer we're supposed to make
-    // var oldDancerMakerFunction = window[oldDancerMakerFunctionName];
-    // console.log('oldDancerMakerFunction', oldDancerMakerFunction);
-
-    // make a dancer with a random position
-
 
     var happyDancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
-
     $('body').append(happyDancer.$node);
   });
 });
